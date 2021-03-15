58730473572faadb2fae4ecf841ce6f259b22cb8
Jesse Ojih <jcinvent05@gmail.com>
2021-03-15 11:16:36 +0100

    Initial commit

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testThatWeCanGetTheFirstName()
+    {
+        $user = new \App\Models\User;
+
+        $user->setFirstName('Billy');
+
+        $this->assertEquals($user->getFirstName(), 'Billy');
+    }
+}
